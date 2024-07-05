31d033e524f87fd9f3d0c0698dd68cd45274795b
Nurul Arifin <nurularifin2796@gmail.com>
2024-07-05 23:37:16 +0700

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './modules/book/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './config/data-source.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    BookModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
