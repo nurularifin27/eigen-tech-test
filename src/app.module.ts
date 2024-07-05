f7c3fbed1b6c166fd0cb4a3523309b06048d09c5
Nurul Arifin <nurularifin2796@gmail.com>
2024-07-05 23:55:27 +0700

    add member

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { BookModule } from './modules/book/book.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './config/data-source.config';
+import { MemberModule } from './modules/member/member.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
-    BookModule
+    BookModule,
+    MemberModule
   ],
   controllers: [AppController],
   providers: [AppService],
